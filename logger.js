bf3ce40b4303b37d8552edbf0e06ebedd832ae48
YongMe <92907633+YongMe@users.noreply.github.com>
2023-04-02 21:10:23 +0530

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,1 +1,13 @@
+const eventEmitter = require('events')
+
+class Logger extends eventEmitter{
+     log(message){
+        console.log('message')
+        eventEmitter.emit('message Logged', {id:1, url:'https://'}
+     }
+     
+}
+
+  
+module.exports = Logger 
 
